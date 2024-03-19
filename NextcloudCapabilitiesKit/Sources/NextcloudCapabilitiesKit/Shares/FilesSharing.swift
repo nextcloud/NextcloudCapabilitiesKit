09c15e3815a9fbe391d7296c3f5002d011bff84f
Claudio Cambra <developer@claudiocambra.com>
2024-03-20 00:37:20 +0800

    Add Email struct to FilesSharing

--- NextcloudCapabilitiesKit/Sources/NextcloudCapabilitiesKit/Shares/FilesSharing.swift
+++ NextcloudCapabilitiesKit/Sources/NextcloudCapabilitiesKit/Shares/FilesSharing.swift
@@ -8,11 +8,13 @@
 import Foundation
 
 public struct FilesSharing {
+    public let email: Email
     public let apiEnabled: Bool
     public let resharing: Bool
     public let defaultPermissions: Int
 
     init() {
+        email = Email()
         apiEnabled = false
         resharing = false
         defaultPermissions = 0
@@ -22,12 +24,14 @@ public struct FilesSharing {
     init(capabilities: [String: Any]) {
         guard let capabilities = capabilities["files_sharing"] as? [String : Any] else {
             debugPrint("No files sharing data in received capabilities.")
+            email = Email()
             apiEnabled = false
             resharing = false
             defaultPermissions = 0
             return
         }
 
+        email = Email(filesSharingCapabilities: capabilities)
         apiEnabled = capabilities["api_enabled"] as? Bool ?? false
         resharing = capabilities["resharing"] as? Bool ?? false
         defaultPermissions = capabilities["default_permissions"] as? Int ?? 0
