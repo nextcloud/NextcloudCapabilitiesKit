7e5402f47a480ae71c55f7e2c95f633eaa62c89c
Claudio Cambra <developer@claudiocambra.com>
2024-03-20 01:12:56 +0800

    Add notifypush struct

---
+++ NextcloudCapabilitiesKit/Sources/NextcloudCapabilitiesKit/NotifyPush/NotifyPush.swift
@@ -0,0 +1,52 @@
+//
+//  NotifyPush.swift
+//
+//
+//  Created by Claudio Cambra on 20/3/24.
+//
+
+import Foundation
+
+public enum PushNotificationType: String {
+    case files = "files"
+    case activities = "activities"
+    case notifications = "notifications"
+}
+
+public struct NotifyPush {
+    public let available: Bool
+    public let types: Set<PushNotificationType>
+    public let endpoints: Endpoints?
+
+    init() {
+        available = true
+        types = []
+        endpoints = nil
+    }
+
+    init(capabilities: [String: Any]) {
+        guard let notifyPushCapabilities = capabilities["notify_push"] as? [String : Any] else {
+            debugPrint("No files sharing data in received capabilities.")
+            available = false
+            types = []
+            endpoints = nil
+            return
+        }
+
+        available = true
+        
+        let receivedTypes = notifyPushCapabilities["type"] as? [String] ?? []
+        var gatheredTypes: Set<PushNotificationType> = []
+        for receivedType in receivedTypes {
+            if receivedType == PushNotificationType.files.rawValue {
+                gatheredTypes.insert(.files)
+            } else if receivedType == PushNotificationType.activities.rawValue {
+                gatheredTypes.insert(.activities)
+            } else if receivedType == PushNotificationType.notifications.rawValue {
+                gatheredTypes.insert(.notifications)
+            }
+        }
+        types = gatheredTypes
+        endpoints = Endpoints(notifyPushCapabilities: notifyPushCapabilities)
+    }
+}
