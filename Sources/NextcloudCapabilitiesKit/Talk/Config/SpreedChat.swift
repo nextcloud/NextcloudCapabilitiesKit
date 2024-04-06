e809a244dbd3dcfdc6c904b4e1d16f6b87a704da
Claudio Cambra <developer@claudiocambra.com>
2024-04-07 00:39:22 +0800

    Add spreed chat struct

---
+++ Sources/NextcloudCapabilitiesKit/Talk/Config/SpreedChat.swift
@@ -0,0 +1,27 @@
+//
+//  SpreedChat.swift
+//  
+//
+//  Created by Claudio Cambra on 7/4/24.
+//
+
+import Foundation
+
+struct SpreedChat {
+    let maxLength: Int
+    let readPrivacy: Int
+    let typingPrivacy: Int
+    let translations: [String]
+
+    init?(spreedConfigCapabilities: [String: Any]) {
+        guard let spreedChatCaps = spreedConfigCapabilities["chat"] as? [String: Any] else {
+            debugPrint("No chat capabilities in spreed config capabilities")
+            return nil
+        }
+
+        maxLength = spreedChatCaps["max-length"] as? Int ?? 0
+        readPrivacy = spreedChatCaps["read-privacy"] as? Int ?? 0
+        typingPrivacy = spreedChatCaps["typing-privacy"] as? Int ?? 0
+        translations = spreedChatCaps["translations"] as? [String] ?? []
+    }
+}
