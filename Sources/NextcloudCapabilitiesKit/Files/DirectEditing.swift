ae544ebdceb5f7ce3cac647a5946c233cd728520
Claudio Cambra <developer@claudiocambra.com>
2024-04-06 23:14:59 +0800

    Add DirectEditing struct

---
+++ Sources/NextcloudCapabilitiesKit/Files/DirectEditing.swift
@@ -0,0 +1,25 @@
+//
+//  DirectEditing.swift
+//
+//
+//  Created by Claudio Cambra on 6/4/24.
+//
+
+import Foundation
+
+struct DirectEditing {
+    public let url: URL?
+    public let etag: String
+    public let supportsFileId: Bool
+
+    init?(filesCapabilities: [String: Any]) {
+        guard let capabilities = filesCapabilities["directEditing"] as? [String : Any] else {
+            debugPrint("No files sharing data in received capabilities.")
+            return nil
+        }
+
+        url = URL(string: (capabilities["url"] as? String ?? ""))
+        etag = capabilities["etag"] as? String ?? ""
+        supportsFileId = capabilities["supportsFileId"] as? Bool ?? false
+    }
+}
