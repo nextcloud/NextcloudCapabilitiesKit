4ddabe48115c130e0f962c14e39b83209f45b502
Claudio Cambra <developer@claudiocambra.com>
2024-04-07 00:39:35 +0800

    add spreed signaling struct

---
+++ Sources/NextcloudCapabilitiesKit/Talk/Config/SpreedSignaling.swift
@@ -0,0 +1,23 @@
+//
+//  SpreedSignalling.swift
+//
+//
+//  Created by Claudio Cambra on 7/4/24.
+//
+
+import Foundation
+
+struct SpreedSignaling {
+    let sessionPingLimit: Int
+    let helloV2TokenKey: String
+
+    init?(spreedConfigCapabilities: [String: Any]) {
+        guard let spreedSignalCaps = spreedConfigCapabilities["signaling"] as? [String: Any] else {
+            debugPrint("No signaling capabilities in spreed config capabilities")
+            return nil
+        }
+
+        sessionPingLimit = spreedSignalCaps["session-ping-limit"] as? Int ?? 0
+        helloV2TokenKey = spreedSignalCaps["hello-v2-token-key"] as? String ?? ""
+    }
+}
