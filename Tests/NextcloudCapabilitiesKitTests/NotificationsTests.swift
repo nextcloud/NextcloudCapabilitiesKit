43ee4a3520d4302b6c9b675207c8352bf54d9f02
Claudio Cambra <developer@claudiocambra.com>
2024-04-08 18:23:39 +0800

    Add tests for notifications

---
+++ Tests/NextcloudCapabilitiesKitTests/NotificationsTests.swift
@@ -0,0 +1,52 @@
+//
+//  NotificationsTests.swift
+//  
+//
+//  Created by Claudio Cambra on 8/4/24.
+//
+
+import XCTest
+@testable import NextcloudCapabilitiesKit
+
+class NotificationsTests: XCTestCase {
+
+    func testNotificationsInitializationWithValidCapabilities() {
+        // Valid notifications capabilities
+        let capabilities: [String: Any] = [
+            "notifications": [
+                "ocs-endpoints": ["endpoint1", "endpoint2"],
+                "push": ["push1", "push2"],
+                "admin-notifications": ["notification1", "notification2"]
+            ]
+        ]
+
+        let notifications = Notifications(capabilities: capabilities)
+        XCTAssertNotNil(notifications, "Notifications instance should be created with valid input")
+        XCTAssertEqual(notifications?.ocsEndpoints, ["endpoint1", "endpoint2"], "Notifications ocsEndpoints should match the provided value")
+        XCTAssertEqual(notifications?.push, ["push1", "push2"], "Notifications push should match the provided value")
+        XCTAssertEqual(notifications?.adminNotifications, ["notification1", "notification2"], "Notifications adminNotifications should match the provided value")
+    }
+
+    func testNotificationsInitializationWithInvalidCapabilities() {
+        // Missing or invalid notifications capabilities
+        let invalidCapabilities: [String: Any] = [:]
+        let notifications = Notifications(capabilities: invalidCapabilities)
+        XCTAssertNil(notifications, "Notifications instance should not be created with missing or invalid input")
+    }
+
+    func testNotificationsInitializationWithPartialInput() {
+        // Partial notifications capabilities
+        let capabilities: [String: Any] = [
+            "notifications": [
+                // Only providing part of the required properties
+                "ocs-endpoints": ["partialEndpoint"]
+            ]
+        ]
+
+        let notifications = Notifications(capabilities: capabilities)
+        XCTAssertNotNil(notifications, "Notifications instance should be created with partial input")
+        XCTAssertEqual(notifications?.ocsEndpoints, ["partialEndpoint"], "Notifications ocsEndpoints should match the provided value with partial input")
+        XCTAssertEqual(notifications?.push, [], "Notifications push should default to an empty array with partial input")
+        XCTAssertEqual(notifications?.adminNotifications, [], "Notifications adminNotifications should default to an empty array with partial input")
+    }
+}
