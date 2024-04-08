7d2c60e5a54e62c970ee2e8760a2065a93bef2fd
Claudio Cambra <developer@claudiocambra.com>
2024-04-08 17:40:41 +0800

    Add tests for NotifyPush

---
+++ Tests/NextcloudCapabilitiesKitTests/NotifyPush/NotifyPushTests.swift
@@ -0,0 +1,51 @@
+//
+//  NotifyPushTests.swift
+//
+//
+//  Created by Claudio Cambra on 8/4/24.
+//
+
+import XCTest
+@testable import NextcloudCapabilitiesKit
+
+class NotifyPushTests: XCTestCase {
+
+    func testNotifyPushInitializationWithValidCapabilities() {
+        // Valid notify push capabilities
+        let validCapabilities: [String: Any] = [
+            "notify_push": [
+                "type": ["files", "activities"]
+            ]
+        ]
+
+        let notifyPush = NotifyPush(capabilities: validCapabilities)
+        XCTAssertNotNil(notifyPush, "NotifyPush instance should be created with valid input")
+        XCTAssertTrue(notifyPush?.available ?? false, "NotifyPush should be available")
+        XCTAssertTrue(notifyPush?.types.contains(.files) ?? false, "NotifyPush types should contain .files")
+        XCTAssertTrue(notifyPush?.types.contains(.activities) ?? false, "NotifyPush types should contain .activities")
+        XCTAssertNil(notifyPush?.endpoints, "NotifyPush endpoints should be nil when not provided")
+    }
+
+    func testNotifyPushInitializationWithUnavailableCapabilities() {
+        // Missing notify push capabilities
+        let invalidCapabilities: [String: Any] = [:]
+        let notifyPush = NotifyPush(capabilities: invalidCapabilities)
+        XCTAssertNil(notifyPush, "NotifyPush instance should not be created with unavailable input")
+    }
+
+    func testNotifyPushInitializationWithPartiallyValidCapabilities() {
+        // Partially valid notify push capabilities
+        let partiallyValidCapabilities: [String: Any] = [
+            "notify_push": [
+                "type": ["files", "invalidType"]
+            ]
+        ]
+
+        let notifyPush = NotifyPush(capabilities: partiallyValidCapabilities)
+        XCTAssertNotNil(notifyPush, "NotifyPush instance should be created with partially valid input")
+        XCTAssertTrue(notifyPush?.available ?? false, "NotifyPush should be available")
+        XCTAssertTrue(notifyPush?.types.contains(.files) ?? false, "NotifyPush types should contain .files")
+        XCTAssertFalse(notifyPush?.types.contains(.notifications) ?? true, "NotifyPush types should not contain .notifications with partially valid input")
+        XCTAssertNil(notifyPush?.endpoints, "NotifyPush endpoints should be nil when not provided")
+    }
+}
