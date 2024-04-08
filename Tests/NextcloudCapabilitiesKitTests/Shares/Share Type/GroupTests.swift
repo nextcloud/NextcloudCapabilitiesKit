9d2075fdbcaf4b6a695411ad5ea75336bf9c2589
Claudio Cambra <developer@claudiocambra.com>
2024-04-08 16:57:05 +0800

    Write tests for Group share type

---
+++ Tests/NextcloudCapabilitiesKitTests/Shares/Share Type/GroupTests.swift
@@ -0,0 +1,35 @@
+//
+//  GroupTests.swift
+//
+//
+//  Created by Claudio Cambra on 8/4/24.
+//
+
+import XCTest
+@testable import NextcloudCapabilitiesKit
+
+final class GroupTests: XCTestCase {
+
+    func testGroupInitialization() {
+        let filesSharingCapabilities: [String: Any] = [
+            "group": [
+                "enabled": true,
+                "expire_date": [
+                    "enabled": false
+                ]
+            ]
+        ]
+
+        let group = Group(filesSharingCapabilities: filesSharingCapabilities)
+
+        XCTAssertNotNil(group, "Group instance should not be nil")
+        XCTAssertEqual(group?.enabled, true, "Enabled should be true")
+        XCTAssertEqual(group?.expireDateEnabled, false, "ExpireDateEnabled should be false")
+    }
+
+    func testGroupInitializationWithMissingData() {
+        let filesSharingCapabilities: [String: Any] = [:] // Missing "group" key
+        let group = Group(filesSharingCapabilities: filesSharingCapabilities)
+        XCTAssertNil(group, "Group initialization should fail with missing data")
+    }
+}
