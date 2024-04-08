161d029eb15ce931a18647b4dcfdd67e8c00baf4
Claudio Cambra <developer@claudiocambra.com>
2024-04-08 18:29:21 +0800

    Add userstatus tests

---
+++ Tests/NextcloudCapabilitiesKitTests/UserStatusTests.swift
@@ -0,0 +1,52 @@
+//
+//  UserStatusTests.swift
+//  
+//
+//  Created by Claudio Cambra on 8/4/24.
+//
+
+import XCTest
+@testable import NextcloudCapabilitiesKit
+
+class UserStatusTests: XCTestCase {
+
+    func testUserStatusInitializationWithValidCapabilities() {
+        // Valid user status capabilities
+        let capabilities: [String: Any] = [
+            "user_status": [
+                "enabled": true,
+                "restore": false,
+                "supports_emoji": true
+            ]
+        ]
+
+        let userStatus = UserStatus(capabilities: capabilities)
+        XCTAssertNotNil(userStatus, "UserStatus instance should be created with valid input")
+        XCTAssertTrue(userStatus?.enabled ?? false, "UserStatus enabled should be true")
+        XCTAssertFalse(userStatus?.restore ?? true, "UserStatus restore should be false")
+        XCTAssertTrue(userStatus?.supportsEmoji ?? false, "UserStatus supportsEmoji should be true")
+    }
+
+    func testUserStatusInitializationWithMissingCapabilities() {
+        // Missing user status capabilities
+        let invalidCapabilities: [String: Any] = [:]
+        let userStatus = UserStatus(capabilities: invalidCapabilities)
+        XCTAssertNil(userStatus, "UserStatus instance should not be created with missing input")
+    }
+
+    func testUserStatusInitializationWithPartialInput() {
+        // Partial user status capabilities
+        let capabilities: [String: Any] = [
+            "user_status": [
+                // Only providing part of the required properties
+                "enabled": false
+            ]
+        ]
+
+        let userStatus = UserStatus(capabilities: capabilities)
+        XCTAssertNotNil(userStatus, "UserStatus instance should be created with partial input")
+        XCTAssertFalse(userStatus?.enabled ?? true, "UserStatus enabled should be false with partial input")
+        XCTAssertFalse(userStatus?.restore ?? true, "UserStatus restore should default to false with partial input")
+        XCTAssertFalse(userStatus?.supportsEmoji ?? true, "UserStatus supportsEmoji should default to false with partial input")
+    }
+}
