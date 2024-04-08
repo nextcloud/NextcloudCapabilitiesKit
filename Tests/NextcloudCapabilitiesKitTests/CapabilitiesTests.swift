a54ee2d0023f82ef68ba6baa371057f9204e308f
Claudio Cambra <developer@claudiocambra.com>
2024-04-08 18:35:45 +0800

    Add tests for capabilities struct

---
+++ Tests/NextcloudCapabilitiesKitTests/CapabilitiesTests.swift
@@ -0,0 +1,45 @@
+//
+//  CapabilitiesTests.swift
+//  
+//
+//  Created by Claudio Cambra on 8/4/24.
+//
+
+import XCTest
+@testable import NextcloudCapabilitiesKit
+
+class CapabilitiesTests: XCTestCase {
+
+    func testCapabilitiesInitializationWithValidData() {
+        // Valid capabilities data
+        let validData = """
+        {
+            "ocs": {
+                "data": {
+                    "capabilities": {
+                        "core": {
+                            "webdav-root": "remote.php/webdav"
+                        },
+                        "dav": {
+                            "chunking": "1.0"
+                        },
+                        "files": {
+                            "bigfilechunking": true
+                        },
+                    }
+                }
+            }
+        }
+        """.data(using: .utf8)!
+
+        let capabilities = Capabilities(data: validData)
+        XCTAssertNotNil(capabilities, "Capabilities instance should be created with valid data")
+    }
+
+    func testCapabilitiesInitializationWithInvalidData() {
+        // Invalid capabilities data
+        let invalidData = Data()
+        let capabilities = Capabilities(data: invalidData)
+        XCTAssertNil(capabilities, "Capabilities instance should not be created with invalid data")
+    }
+}
