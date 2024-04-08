50e8f54d2ff1eb50df0905ef4023fbcbc8f95b57
Claudio Cambra <developer@claudiocambra.com>
2024-04-08 16:57:30 +0800

    Write tests for user share type

---
+++ Tests/NextcloudCapabilitiesKitTests/Shares/Share Type/UserTests.swift
@@ -0,0 +1,49 @@
+//
+//  UserTests.swift
+//  
+//
+//  Created by Claudio Cambra on 8/4/24.
+//
+
+import XCTest
+@testable import NextcloudCapabilitiesKit
+
+class UserTests: XCTestCase {
+
+    func testValidUserInitialization() {
+        // Valid user data with all required subobjects
+        let validFilesSharingCapabilities: [String: Any] = [
+            "user": [
+                "send_mail": true,
+                "expire_date": [
+                    "enabled": true
+                ]
+            ]
+        ]
+
+        let user = User(filesSharingCapabilities: validFilesSharingCapabilities)
+        XCTAssertNotNil(user, "User instance should be created with valid input")
+        XCTAssertEqual(user?.sendMail, true, "SendMail should be true")
+        XCTAssertEqual(user?.expireDateEnabled, true, "ExpireDateEnabled should be true")
+    }
+
+    func testInvalidUserInitialization() {
+        let invalidFilesSharingCapabilities: [String: Any] = [:]
+        let user = User(filesSharingCapabilities: invalidFilesSharingCapabilities)
+        XCTAssertNil(user, "User instance should not be created with invalid input")
+    }
+
+    func testPartiallyValidUserInitialization() {
+        let partialFilesSharingCapabilities: [String: Any] = [
+            "user": [
+                "send_mail": true,
+                // Missing "expire_date" subobject
+            ]
+        ]
+
+        let user = User(filesSharingCapabilities: partialFilesSharingCapabilities)
+        XCTAssertNotNil(user, "User instance should be created even with partially valid input")
+        XCTAssertEqual(user?.sendMail, true, "SendMail should be true")
+        XCTAssertEqual(user?.expireDateEnabled, false, "ExpireDateEnabled should default to false when subobject is missing")
+    }
+}
