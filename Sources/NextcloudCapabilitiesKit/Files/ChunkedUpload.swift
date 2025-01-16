a07309e13a954db5892ff39314f57137229062f7
Claudio Cambra <developer@claudiocambra.com>
2025-01-16 16:23:08 +0800

    Add ChunkedUpload struct
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

---
+++ Sources/NextcloudCapabilitiesKit/Files/ChunkedUpload.swift
@@ -0,0 +1,24 @@
+//
+//  ChunkedUpload.swift
+//
+//
+//  Created by Claudio Cambra on 16/1/25.
+//
+
+
+import Foundation
+
+struct ChunkedUpload {
+    public let maxParallelCount: Int64
+    public let maxChunkSize: Int64
+
+    init?(filesCapabilities: [String: Any]) {
+        guard let capabilities = filesCapabilities["chunked_upload"] as? [String : Any] else {
+            debugPrint("No chunked upload data in received capabilities.")
+            return nil
+        }
+
+        maxParallelCount = Int64(capabilities["max_parallel_count"] as? Int ?? -1)
+        maxChunkSize = Int64(capabilities["max_size"] as? Int ?? -1)
+    }
+}
