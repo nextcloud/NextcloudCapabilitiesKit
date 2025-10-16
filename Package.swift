4609ef55f80a6958333074e1f5658992a00ce373
Iva Horn <iva.horn@nextcloud.com>
2025-10-16 14:56:56 +0200

    fix: Enabled strict concurrency checks in Swift.

--- Package.swift
+++ Package.swift
@@ -9,7 +9,8 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "NextcloudCapabilitiesKit",
-            targets: ["NextcloudCapabilitiesKit"]),
+            targets: ["NextcloudCapabilitiesKit"],
+        ),
     ],
     dependencies: [
         .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.55.0"),
@@ -18,9 +19,17 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "NextcloudCapabilitiesKit"),
+            name: "NextcloudCapabilitiesKit",
+            swiftSettings: [
+              .enableUpcomingFeature("StrictConcurrency")
+            ]
+        ),
         .testTarget(
             name: "NextcloudCapabilitiesKitTests",
-            dependencies: ["NextcloudCapabilitiesKit"]),
-    ]
+            dependencies: ["NextcloudCapabilitiesKit"],
+            swiftSettings: [
+              .enableUpcomingFeature("StrictConcurrency")
+            ]
+        ),
+    ],
 )
