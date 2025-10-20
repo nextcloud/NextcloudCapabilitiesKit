07a481be18943cfa3ce17c91b83d6017cdbb5846
Iva Horn <iva.horn@nextcloud.com>
2025-10-20 09:58:55 +0200

    fix: Removed the strict concurrency swift setting from package manifest.
    Our CI tolling was complaining about it: error: upcoming feature 'StrictConcurrency' is already enabled as of Swift version 6

--- Package.swift
+++ Package.swift
@@ -9,30 +9,13 @@ import PackageDescription
 let package = Package(
     name: "NextcloudCapabilitiesKit",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "NextcloudCapabilitiesKit",
-            targets: ["NextcloudCapabilitiesKit"]
-        )
+        .library(name: "NextcloudCapabilitiesKit", targets: ["NextcloudCapabilitiesKit"])
     ],
     dependencies: [
         .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.55.0")
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "NextcloudCapabilitiesKit",
-            swiftSettings: [
-                .enableUpcomingFeature("StrictConcurrency")
-            ]
-        ),
-        .testTarget(
-            name: "NextcloudCapabilitiesKitTests",
-            dependencies: ["NextcloudCapabilitiesKit"],
-            swiftSettings: [
-                .enableUpcomingFeature("StrictConcurrency")
-            ]
-        )
+        .target(name: "NextcloudCapabilitiesKit"),
+        .testTarget(name: "NextcloudCapabilitiesKitTests", dependencies: ["NextcloudCapabilitiesKit"])
     ]
 )
