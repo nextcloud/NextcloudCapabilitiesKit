0a104ff7bc2ecfe79bb87abcee3e635fb3e61329
Claudio Cambra <developer@claudiocambra.com>
2026-08-19 13:58:09 +0800

    Add missing proprties to Public Link share capabilities
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- Sources/NextcloudCapabilitiesKit/Shares/Share Type/PublicLink.swift
+++ Sources/NextcloudCapabilitiesKit/Shares/Share Type/PublicLink.swift
@@ -7,12 +7,17 @@ public struct PublicLink: Equatable, Sendable {
     public let enabled: Bool
     public let allowUpload: Bool
     public let supportsUploadOnly: Bool
+    public let sendMail: Bool
+    public let customTokens: Bool
     public let askOptionalPassword: Bool
     public let passwordEnforced: Bool
+    public let expireDateEnabled: Bool
     public let expireDateEnforced: Bool
     public let expireDateDays: Int
+    public let internalExpireDateEnabled: Bool
     public let internalExpireDateEnforced: Bool
     public let internalExpireDateDays: Int
+    public let remoteExpireDateEnabled: Bool
     public let remoteExpireDateEnforced: Bool
     public let remoteExpireDateDays: Int
     public let multipleAllowed: Bool
@@ -25,8 +30,14 @@ public struct PublicLink: Equatable, Sendable {
 
         enabled = publicLinkCaps["enabled"] as? Bool ?? false
         allowUpload = publicLinkCaps["upload"] as? Bool ?? false
-        supportsUploadOnly = publicLinkCaps["supports_upload_only"] as? Bool ?? false
-        multipleAllowed = publicLinkCaps["multiple"] as? Bool ?? false
+        supportsUploadOnly = publicLinkCaps["upload_files_drop"] as? Bool
+            ?? publicLinkCaps["supports_upload_only"] as? Bool
+            ?? false
+        sendMail = publicLinkCaps["send_mail"] as? Bool ?? false
+        customTokens = publicLinkCaps["custom_tokens"] as? Bool ?? false
+        multipleAllowed = publicLinkCaps["multiple_links"] as? Bool
+            ?? publicLinkCaps["multiple"] as? Bool
+            ?? false
 
         if let passwordCaps = publicLinkCaps["password"] as? [String: Any] {
             askOptionalPassword = passwordCaps["askForOptionalPassword"] as? Bool ?? false
@@ -37,25 +48,31 @@ public struct PublicLink: Equatable, Sendable {
         }
 
         if let expireDateCapabilities = publicLinkCaps["expire_date"] as? [String: Any] {
+            expireDateEnabled = expireDateCapabilities["enabled"] as? Bool ?? false
             expireDateDays = expireDateCapabilities["days"] as? Int ?? 1
             expireDateEnforced = expireDateCapabilities["enforced"] as? Bool ?? false
         } else {
+            expireDateEnabled = false
             expireDateDays = 1
             expireDateEnforced = false
         }
 
         if let internalExpDateCaps = publicLinkCaps["expire_date_internal"] as? [String: Any] {
+            internalExpireDateEnabled = internalExpDateCaps["enabled"] as? Bool ?? false
             internalExpireDateDays = internalExpDateCaps["days"] as? Int ?? 1
             internalExpireDateEnforced = internalExpDateCaps["enforced"] as? Bool ?? false
         } else {
+            internalExpireDateEnabled = false
             internalExpireDateDays = 1
             internalExpireDateEnforced = false
         }
 
         if let remoteExpDateCaps = publicLinkCaps["expire_date_remote"] as? [String: Any] {
+            remoteExpireDateEnabled = remoteExpDateCaps["enabled"] as? Bool ?? false
             remoteExpireDateDays = remoteExpDateCaps["days"] as? Int ?? 1
             remoteExpireDateEnforced = remoteExpDateCaps["enforced"] as? Bool ?? false
         } else {
+            remoteExpireDateEnabled = false
             remoteExpireDateDays = 1
             remoteExpireDateEnforced = false
         }
