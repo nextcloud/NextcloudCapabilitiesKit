5fc11e91cb42773cd8ace86d9e7e2fe8d1dfff6b
Claudio Cambra <developer@claudiocambra.com>
2026-08-19 13:57:52 +0800

    Add missing properties to Email share capabilities
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- Sources/NextcloudCapabilitiesKit/Shares/Share Type/Email.swift
+++ Sources/NextcloudCapabilitiesKit/Shares/Share Type/Email.swift
@@ -4,8 +4,13 @@
 import Foundation
 
 public struct Email: Equatable, Sendable {
+    public let enabled: Bool
+    public let sendsPasswordByEmail: Bool
+    public let uploadFilesDropEnabled: Bool
     public let passwordEnabled: Bool
     public let passwordEnforced: Bool
+    public let expireDateEnabled: Bool
+    public let expireDateEnforced: Bool
 
     init?(filesSharingCapabilities: [String: Any]) {
         guard let emailCaps = filesSharingCapabilities["sharebymail"] as? [String: Any] else {
@@ -13,12 +18,24 @@ public struct Email: Equatable, Sendable {
             return nil
         }
 
+        enabled = emailCaps["enabled"] as? Bool ?? false
+        sendsPasswordByEmail = emailCaps["send_password_by_mail"] as? Bool ?? false
+        uploadFilesDropEnabled = (emailCaps["upload_files_drop"] as? [String: Any])?["enabled"] as? Bool ?? false
+
         if let passwordCapabilities = emailCaps["password"] as? [String: Any] {
             passwordEnabled = passwordCapabilities["enabled"] as? Bool ?? false
             passwordEnforced = passwordCapabilities["enforced"] as? Bool ?? false
         } else {
             passwordEnabled = false
             passwordEnforced = false
+        }
+
+        if let expireDateCapabilities = emailCaps["expire_date"] as? [String: Any] {
+            expireDateEnabled = expireDateCapabilities["enabled"] as? Bool ?? false
+            expireDateEnforced = expireDateCapabilities["enforced"] as? Bool ?? false
+        } else {
+            expireDateEnabled = false
+            expireDateEnforced = false
         }
     }
 }
