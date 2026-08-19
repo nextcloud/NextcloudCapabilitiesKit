31c22a078c6a93f5f72446d3a1dda3e70150d26e
Claudio Cambra <developer@claudiocambra.com>
2026-08-19 13:57:52 +0800

    Add missing properties to Email share capabilities
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- Tests/NextcloudCapabilitiesKitTests/Shares/Share Type/EmailTests.swift
+++ Tests/NextcloudCapabilitiesKitTests/Shares/Share Type/EmailTests.swift
@@ -9,17 +9,31 @@ final class EmailTests: XCTestCase {
         // Prepare test data
         let filesSharingCapabilities: [String: Any] = [
             "sharebymail": [
+                "enabled": true,
+                "send_password_by_mail": true,
+                "upload_files_drop": [
+                    "enabled": true
+                ],
                 "password": [
                     "enabled": true,
                     "enforced": false
+                ],
+                "expire_date": [
+                    "enabled": true,
+                    "enforced": true
                 ]
             ]
         ]
 
         let email = Email(filesSharingCapabilities: filesSharingCapabilities)
         XCTAssertNotNil(email)
+        XCTAssertEqual(email?.enabled, true)
+        XCTAssertEqual(email?.sendsPasswordByEmail, true)
+        XCTAssertEqual(email?.uploadFilesDropEnabled, true)
         XCTAssertEqual(email?.passwordEnabled, true)
         XCTAssertEqual(email?.passwordEnforced, false)
+        XCTAssertEqual(email?.expireDateEnabled, true)
+        XCTAssertEqual(email?.expireDateEnforced, true)
     }
 
     func testEmailInitializationWithMissingData() {
